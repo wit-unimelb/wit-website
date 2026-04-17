a44766276da706839d3968a45b97794dede95015
exyw <emmax@student.unimelb.edu.au>
2026-04-17 19:45:51 +1000

    temporarily removed WIP pages from website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,10 @@ export default function Navbar() {
         <nav>
             <a className="nav-logo" href="/"><span>WIT</span> @ UniMelb</a>
             <ul className={isOpen ? 'nav-links open' : 'nav-links'}>
-                <li><Link to="/about">About Us</Link></li>
+                {/* <li><Link to="/about">About Us</Link></li> */}
                 <li><Link to="/events">Events</Link></li>
                 <li><Link to="/sponsors">Sponsors</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                {/* <li><Link to="/contact">Contact</Link></li> */}
                 <li><a href="https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/website/" className="cta"
                     target="_blank" rel="noopener noreferrer">Join Us</a></li>
             </ul>
