c0b28acbabb2770be3eb60ca35a1b2d24050cf9b
Kylie Lam <kylielam@student.unimelb.edu.au>
2026-09-08 13:31:39 +1000

    added sponsor images to the marquee

--- src/components/home/Marquee.tsx
+++ src/components/home/Marquee.tsx
@@ -3,20 +3,34 @@
 import './Marquee.css'
 
 const sponsors = [
-    'Airwallex', 'Macquarie', 'Jane Street', 'Fenwick',
-    'Atlassian', 'Australian Super', 'Susquehanna', 'CBA',
-    'Vanguard', 'Optiver', 'Accenture', 'PWC',
-    'Seek', 'EY',
+    { name: 'Airwallex', logo: '../sponsor-images/airwallex.png'},
+    { name: 'Macquarie', logo: '../sponsor-images/macquarie.png'},
+    { name: 'Jane Street', logo: '../sponsor-images/janestreet.png'},
+    { name: 'Fenwick', logo: '../sponsor-images/fenwick.png'},
+    { name: 'Atlassian', logo: '../sponsor-images/atlassian.png'},
+    { name: 'Australian Super', logo: '../sponsor-images/australiansuper.png'},
+    { name: 'Susquehanna', logo: '../sponsor-images/susquehanna.png'},
+    { name: 'CBA', logo: '../sponsor-images/cba.png'},
+    { name: 'Vanguard', logo: '../sponsor-images/vanguard.png'},
+    { name: 'Optiver', logo: '../sponsor-images/optiver.png'},
+    { name: 'Accenture', logo: '../sponsor-images/accenture.png'},
+    { name: 'PWC', logo: '../sponsor-images/pwc.png'},
+    { name: 'Seek', logo: '../sponsor-images/seek.png'},
+    { name: 'EY', logo: '../sponsor-images/ey.png'},
 ]
 
 export default function Marquee() {
     return (
        <div className="sponsors-row">
-            <p className="sponsors-kicker">OUR SPONSORS</p>
+             <p className="sponsors-kicker">OUR SPONSORS</p>
             <div style={{ overflow: 'hidden', padding: '20px 40px' }}>
                 <div className="marquee-track">
-                    {[...sponsors, ...sponsors].map((name, i) => (
-                        <span className="sponsor-pill" key={i}>{name}</span>
+                    {[...sponsors, ...sponsors].map((sponsor, i) => (
+                        <img 
+                        key={`${sponsor.name}-${i}`}
+                        src={sponsor.logo}
+                        alt={ `${sponsor.name}`}
+                        />
                     ))}
                 </div>
             </div>
